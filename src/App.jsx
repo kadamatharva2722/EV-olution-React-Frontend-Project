a71fe6896e0973c2dfdc16986aeb09655404ebfc
Atharva Kadam <kadamatharva04@gmail.com>
2025-09-23 21:25:50 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Background from './component/background/background'
+import Navbar from "./component/navbar/navbar";
+import Hero from "./component/hero/hero";
+
+const App = () => {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => { return count === 2 ? 0 : count + 1 })
+    }, 3000)
+  }, [])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App;
