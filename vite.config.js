78054c25f672af2ec8be1c372bb7e813d3816ee0
Atharva Kadam <kadamatharva04@gmail.com>
2025-10-18 10:30:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EV-olution-React-Frontend-Project/'  // <--- important for GitHub Pages
 })
